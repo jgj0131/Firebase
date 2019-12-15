d4b2a262597cba772378e10281302db2eb67a392
gukjin jang <jgj455@gmail.com>
2019-12-15 19:19:00 +0900

    Add: Project 생성

---
+++ Firebase/Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by jang gukjin on 2019/12/15.
+//  Copyright © 2019 jang gukjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
